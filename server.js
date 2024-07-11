45b7edee31202f6643e5698a99ddc4574fab7c7c
Maia <matteusmaia889@gmail.com>
2024-07-11 19:45:53 -0300

    feat: create video route

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import { fastify } from "fastify";
+import {DatabaseMemory} from 'database-memory.js'
 
 const server = fastify()
+const database = new DatabaseMemory()
 
 server.get('/videos', (req, res) => {
-  console.log("/  route")
+  
 })
 
 server.post('/videos', () => {
-  console.log("/hello  route")
+  database.create(
+    {
+      title: 'Primeiro video',
+      description: 'lorem ipsum dolor sit amet, consectetur adip',
+      duration: 180
+    }
+  )
 })
 server.put('/videos/:id', () => {
 
