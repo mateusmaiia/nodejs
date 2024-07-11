f4f099e9fde954911d0d67c0c985f2ddea371aed
Maia <matteusmaia889@gmail.com>
2024-07-11 19:42:58 -0300

    feat: server routes

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ import { fastify } from "fastify";
 
 const server = fastify()
 
+server.get('/videos', (req, res) => {
+  console.log("/  route")
+})
+
+server.post('/videos', () => {
+  console.log("/hello  route")
+})
+server.put('/videos/:id', () => {
+
+})
+server.delete('/videos/:id', () => {
+
+})
+
 server.listen({
   port: 1889
 })
