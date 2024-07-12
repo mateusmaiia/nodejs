6d62757e1cddcace3ec035c8f52d81e88dfc8f11
Maia <matteusmaia889@gmail.com>
2024-07-12 03:12:28 -0300

    database connection

---
+++ db.js
@@ -0,0 +1,18 @@
+// app.js
+import postgres from 'postgres';
+import 'dotenv/config'
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+PGPASSWORD = decodeURIComponent(PGPASSWORD);
+
+export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
