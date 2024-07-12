51d72f081517be98e1be84cca0a4552a79ea3028
Maia <matteusmaia889@gmail.com>
2024-07-11 21:01:25 -0300

    feat: rotas backend

--- server.js
+++ server.js
@@ -1,27 +1,51 @@
 import { fastify } from "fastify";
-import {DatabaseMemory} from 'database-memory.js'
+import { DatabaseMemory } from "./database-memory.js";
 
-const server = fastify()
 const database = new DatabaseMemory()
+const server = fastify()
 
 server.get('/videos', (req, res) => {
+  const videos = database.list()
   
+  console.log(videos)
+
+  return videos
 })
 
-server.post('/videos', () => {
+server.post('/videos', (req, reply) => {
+  const {title, description, duration} = req.body
+
   database.create(
     {
-      title: 'Primeiro video',
-      description: 'lorem ipsum dolor sit amet, consectetur adip',
-      duration: 180
+      title,
+      description,
+      duration
     }
   )
-})
-server.put('/videos/:id', () => {
 
+  return reply.status(201).send()
 })
-server.delete('/videos/:id', () => {
+server.put('/videos/:id', (req, res) => {
+  const videoId = req.params.id
+  const {title, description, duration} = req.body
+  
+  database.update(
+    videoId,
+    {
+      title,
+      description,
+      duration
+    }
+  )
 
+  return res.status(204).send()
+})
+server.delete('/videos/:id', (req, res) => {
+  const id = req.params.id
+
+  database.delete(id)
+
+  return res.status(204).send()
 })
 
 server.listen({
