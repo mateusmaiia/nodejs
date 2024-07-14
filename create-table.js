cb90ae25b663eff2e8db07483ebb99483473ef6e
Maia <matteusmaia889@gmail.com>
2024-07-14 15:15:05 -0300

    create and drop table

--- create-table.js
+++ create-table.js
@@ -1,13 +1,20 @@
-import { sql } from "./db.js";
+import { sql } from './db.js';
+
+// sql`
+//   DROP TABLE IF EXISTS videos;
+// `.then(() => {
+//   console.log("Table 'videos' deleted successfully");
+// })
 
 sql`
   CREATE TABLE videos (
+    id TEXT PRIMARY KEY,
     title TEXT,
     description TEXT,
-    duration INTEGER 
+    duration INTEGER
   );
 `.then(() => {
   console.log("Table 'videos' created successfully");
-}).catch((err) => {
-  console.log(err);
-})
+}).catch(error => {
+  console.error("Error creating table 'videos':", error);
+});
